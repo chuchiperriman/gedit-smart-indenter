77abb6f7d311159c8ea35b8c49fc0118ee6caf03
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2009-10-11 17:05:22 +0200

    Adapting the project

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="gedit-sc-plugin"
+PKG_NAME="gedit-smart-indenter"
 
 (test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
