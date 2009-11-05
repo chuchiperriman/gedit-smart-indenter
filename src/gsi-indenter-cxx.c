ea6cb52b1fc911b59778035296f4254b1d0018b7
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2009-11-06 00:01:27 +0100

    Created cxx indenter structure

---
+++ src/gsi-indenter-cxx.c
@@ -0,0 +1,86 @@
+/* gsi-indenter-cxx.c */
+
+#include "gsi-indenter-cxx.h"
+#include "gsi-indenter-utils.h"
+
+#define INDENTER_CXX_PRIVATE(o) \
+	(G_TYPE_INSTANCE_GET_PRIVATE ((o), GSI_TYPE_INDENTER_CXX, GsiIndenterCxxPrivate))
+  
+typedef struct _GsiIndenterCxxPrivate GsiIndenterCxxPrivate;
+
+struct _GsiIndenterCxxPrivate
+{
+	gboolean dummy;
+};
+
+static void gsi_indenter_iface_init (gpointer g_iface, gpointer iface_data);
+
+G_DEFINE_TYPE_WITH_CODE (GsiIndenterCxx,
+                         gsi_indenter_cxx,
+                         G_TYPE_OBJECT,
+                         G_IMPLEMENT_INTERFACE (GSI_TYPE_INDENTER,
+                                                gsi_indenter_iface_init))
+
+static void
+gsi_indenter_indent_line_impl (GsiIndenter *indenter,
+			       GtkTextView *view,
+			       GtkTextIter *iter)
+{
+	
+}
+
+static void
+gsi_indenter_indent_new_line_impl (GsiIndenter *indenter,
+				   GtkTextView *view,
+				   GtkTextIter *iter)
+{
+
+}
+
+static void
+gsi_indenter_indent_region_impl (GsiIndenter *indenter,
+				 GtkTextView *view,
+				 GtkTextIter *start,
+				 GtkTextIter *end)
+{
+
+}
+
+static void
+gsi_indenter_iface_init (gpointer g_iface,
+                         gpointer iface_data)
+{
+        GsiIndenterInterface *iface = (GsiIndenterInterface *)g_iface;
+
+        /* Interface data getter implementations */
+        iface->indent_new_line = gsi_indenter_indent_new_line_impl;
+        iface->indent_line = gsi_indenter_indent_line_impl;
+        iface->indent_region = gsi_indenter_indent_region_impl;
+}
+
+static void
+gsi_indenter_cxx_dispose (GObject *object)
+{
+	G_OBJECT_CLASS (gsi_indenter_cxx_parent_class)->dispose (object);
+}
+
+static void
+gsi_indenter_cxx_class_init (GsiIndenterCxxClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+	
+	g_type_class_add_private (klass, sizeof (GsiIndenterCxxPrivate));
+	
+	object_class->dispose = gsi_indenter_cxx_dispose;
+}
+
+static void
+gsi_indenter_cxx_init (GsiIndenterCxx *self)
+{
+}
+
+GsiIndenterCxx*
+gsi_indenter_cxx_new (void)
+{
+	return g_object_new (GSI_TYPE_INDENTER_CXX, NULL);
+}
---
+++ src/gsi-indenter-cxx.h
@@ -0,0 +1,46 @@
+/* gsi-indenter-cxx.h */
+
+#ifndef _GSI_INDENTER_CXX_H
+#define _GSI_INDENTER_CXX_H
+
+#include "gsi-indenter.h"
+
+G_BEGIN_DECLS
+
+#define GSI_TYPE_INDENTER_CXX gsi_indenter_cxx_get_type()
+
+#define GSI_INDENTER_CXX(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj), \
+  GSI_TYPE_INDENTER_CXX, GsiIndenterCxx))
+
+#define GSI_INDENTER_CXX_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST ((klass), \
+  GSI_TYPE_INDENTER_CXX, GsiIndenterCxxClass))
+
+#define GSI_IS_INDENTER_CXX(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), \
+  GSI_TYPE_INDENTER_CXX))
+
+#define GSI_IS_INDENTER_CXX_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE ((klass), \
+  GSI_TYPE_INDENTER_CXX))
+
+#define GSI_INDENTER_CXX_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), \
+  GSI_TYPE_INDENTER_CXX, GsiIndenterCxxClass))
+
+typedef struct {
+  GObject parent;
+} GsiIndenterCxx;
+
+typedef struct {
+  GObjectClass parent_class;
+} GsiIndenterCxxClass;
+
+GType gsi_indenter_cxx_get_type (void);
+
+GsiIndenterCxx* gsi_indenter_cxx_new (void);
+
+G_END_DECLS
+
+#endif /* _GSI_INDENTER_CXX_H */
